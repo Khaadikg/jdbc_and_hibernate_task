89babc627226b307504eacea1e5d114b30294ed0
Khaadikg <126019047+Khaadikg@users.noreply.github.com>
2023-03-30 16:53:52 +0300

    finished

---
+++ JDBCAndHibernateTask/jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -0,0 +1,11 @@
+package peaksoft;
+
+import peaksoft.dao.UserDaoJdbcImpl;
+import peaksoft.model.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        // Here goes the magic!
+    }
+}
